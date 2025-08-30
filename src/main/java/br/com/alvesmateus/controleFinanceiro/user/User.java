e9294e7865e82c67203cef5aa69a3380a627f312
Mateus Alves <mateus-pc@MATEUS-DESKTOP.localdomain>
2025-08-30 00:07:53 -0300

    feat: create User model

---
+++ src/main/java/br/com/alvesmateus/controleFinanceiro/user/User.java
@@ -0,0 +1,27 @@
+package br.com.alvesmateus.controleFinanceiro.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+
+    private String password;
+}
