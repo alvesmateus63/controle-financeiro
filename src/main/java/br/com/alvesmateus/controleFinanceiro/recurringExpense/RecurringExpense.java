47e076a3ec2c19f7b5dfea5b14b2d8f28a847a05
Mateus Alves <mateus-pc@MATEUS-DESKTOP.localdomain>
2025-08-30 00:08:37 -0300

    feat: create RecurringExpense model

---
+++ src/main/java/br/com/alvesmateus/controleFinanceiro/recurringExpense/RecurringExpense.java
@@ -0,0 +1,33 @@
+package br.com.alvesmateus.controleFinanceiro.recurringExpense;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecurringExpense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private Long value;
+
+    private LocalDate dueDate;
+
+    private Boolean active;
+
+}
