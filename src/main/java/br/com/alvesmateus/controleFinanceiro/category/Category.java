969e645a27eb11796d446d8f38b5f635e2a7c7f8
Mateus Alves <mateus-pc@MATEUS-DESKTOP.localdomain>
2025-08-30 00:08:28 -0300

    feat: create Category model

---
+++ src/main/java/br/com/alvesmateus/controleFinanceiro/category/Category.java
@@ -0,0 +1,28 @@
+package br.com.alvesmateus.controleFinanceiro.category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private UUID idUser;
+}
